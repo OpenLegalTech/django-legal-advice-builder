0dc7d468887e48b493c9e9688886fd9f99ec462f
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-04-30 15:29:23 +0200

    Add some more tests

--- legal_advice_builder/tests/test_views.py
+++ legal_advice_builder/tests/test_views.py
@@ -4,7 +4,7 @@ from django.contrib.sessions.middleware import SessionMiddleware
 from legal_advice_builder.models import LawCase, Question
 from legal_advice_builder.views import FormWizardView
 
-from .helpers import get_single_option_question, get_text_question
+from .helpers import get_single_option_question
 
 
 @pytest.mark.django_db
@@ -20,26 +20,16 @@ def test_form_wizard_view(rf, law_case_factory, questionaire_factory):
         law_case=law_case,
         order=1
     )
-    qn_2 = questionaire_factory(
-        law_case=law_case,
-        order=2
-    )
     q1 = Question.add_root(
         **(get_single_option_question(
             questionaire=qn_1)))
-    q1.success_options = ['unsure']
-    q1.add_child(
+    q2 = q1.add_child(
         **(get_single_option_question(
-            questionaire=qn_1,
-            parent_option='yes')))
-    q1.add_child(
-        **(get_single_option_question(
-            questionaire=qn_1,
-            parent_option='no')))
-    Question.add_root(
-        **(get_text_question(
-            questionaire=qn_2
-        )))
+           questionaire=qn_1,
+           parent_option='yes')))
+
+    q2.failure_options = ['no']
+    q2.save()
 
     request = rf.get('/')
     middleware = SessionMiddleware()
@@ -49,8 +39,49 @@ def test_form_wizard_view(rf, law_case_factory, questionaire_factory):
     assert 'legal_advice_builder/form_wizard.html' in response.template_name
     assert response.context_data.get('form').fields['question'].initial == q1.id
     dict_as_choices = [(k, v) for k, v in q1.options.items()]
-    praefix = 'legal_advice_builder_{}'.format(law_case.id)
     assert response.context_data.get('form').fields['option'].choices == dict_as_choices
+
+    praefix = 'legal_advice_builder_{}'.format(law_case.id)
+
+    session_data = response._request.session.get(praefix)
     assert response._request.session.get(praefix).get('current_questionaire') == qn_1.id
     assert response._request.session.get(praefix).get('current_question') == q1.id
     assert response._request.session.get(praefix).get('answers') == []
+
+    data = {
+        'question': q1.id,
+        'option': 'yes'
+    }
+
+    request = rf.post('/', data)
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+    request.session.save()
+    request.session[praefix] = session_data
+    response = TestWizardView.as_view()(request)
+
+    assert 'legal_advice_builder/form_wizard.html' in response.template_name
+    assert response.context_data.get('form').fields['question'].initial == q2.id
+    assert response._request.session.get(praefix).get('current_questionaire') == qn_1.id
+    assert response._request.session.get(praefix).get('current_question') == q2.id
+    assert response._request.session.get(praefix).get('answers') == [{
+        'option': 'yes', 'question': '1'
+    }]
+
+    session_data = response._request.session.get(praefix)
+
+    data = {
+        'question': q2.id,
+        'option': 'no'
+    }
+
+    request = rf.post('/', data)
+    middleware = SessionMiddleware()
+    middleware.process_request(request)
+    request.session.save()
+    request.session[praefix] = session_data
+    response = TestWizardView.as_view()(request)
+
+    assert 'legal_advice_builder/form_wizard.html' in response.template_name
+    assert response.context_data.get('failure') is True
+    assert response.context_data.get('form') is None
