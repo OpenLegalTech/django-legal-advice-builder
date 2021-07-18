87f51ab2001b3296c85db3857eac7402324b4749
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-07-18 17:27:39 +0200

    Fix bug

--- legal_advice_builder/forms.py
+++ legal_advice_builder/forms.py
@@ -197,6 +197,8 @@ class QuestionConditionForm(forms.ModelForm):
                         if question:
                             condition['then_value'] = 'question'
                             condition['then_question'] = question
+                    else:
+                        condition.pop('then_question')
                     condition = Condition.objects.create(**condition)
         if 'failure_message' in self.cleaned_data:
             self.instance.failure_message = self.cleaned_data['failure_message']
