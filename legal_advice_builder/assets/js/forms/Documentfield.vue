445993ec68931a421190861830b582993b5ed78b
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-08-11 13:15:02 +0200

    Improve textblock editing

--- legal_advice_builder/assets/js/forms/Documentfield.vue
+++ legal_advice_builder/assets/js/forms/Documentfield.vue
@@ -1,108 +1,61 @@
 <template>
   <div>
     <div
-      @mouseover="toggleHover"
+      @mouseenter="toggleHover"
       @mouseleave="toggleHover"
-      @click="toggleShowForm"
       v-bind:style="{ cursor: 'pointer' }"
-      class="bg-light position-relative my-2"
+      class="position-relative my-2 px-3 pt-3 pb-1"
+      :class="{
+        'bg-light': hover && !showForm && !showConditionForm,
+        'bg-primary': showForm || showConditionForm,
+      }"
     >
       <div
-        v-if="this.newQuestion !== '' && this.newIfValue !== '' && !showForm"
+        v-if="!showForm && !showConditionForm && hover"
         class="
           position-absolute
-          top-50
+          btn-group
+          border
+          top-0
           start-50
           translate-middle
-          bg-secondary
-          text-white
-          p-1
         "
+        role="group"
+        aria-label="Basic example"
       >
-        <small>Condition</small>
-        <p class="mb-0">{{ this.getText() }} - {{ this.newIfValue }}</p>
+        <button
+          title="edit text"
+          type="button"
+          @click="toggleShowForm"
+          class="btn bg-white text-body btn-sm"
+        >
+          <i class="bi bi-pencil"></i>
+        </button>
+        <button
+          title="edit condition"
+          type="button"
+          @click="toggleConditionShowForm"
+          class="btn bg-white text-body btn-sm"
+        >
+          <i class="bi bi-option"></i>
+        </button>
+        <button
+          title="delete textblock"
+          type="button"
+          class="btn bg-white text-body btn-sm"
+        >
+          <i class="bi bi-trash"></i>
+        </button>
       </div>
+
       <div
-        v-if="!showForm"
+        v-if="!showForm && !showConditionForm"
         :style="getContentStyles()"
         ref="contentBox"
         v-html="renderedContent"
       ></div>
-    </div>
 
-    <div v-if="showForm" class="card">
-      <div class="card-header text-end p-1 border-0">
-        <span
-          @click="toggleShowForm"
-          :style="{ cursor: 'pointer' }"
-          class="badge rounded-pill bg-transparent text-body"
-        >
-          <i class="bi bi-x-lg"></i>
-        </span>
-      </div>
-      <div class="card-body bg-light">
-        <div
-          v-if="showForm && !showConditionForm && !newQuestion && !newIfValue"
-          class="bg-secondary text-white p-2"
-        >
-          <small>Condition</small>
-          <p class="mb-0">This textblock is always displayed</p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-2">
-            <button @click="toggleConditionShowForm" class="btn btn-outline-light btn-sm" type="button">
-              <i class="bi bi-pencil-fill"></i>
-            </button>
-          </div>
-        </div>
-
-        <div
-          v-if="showForm && !showConditionForm && newQuestion && newIfValue"
-          class="bg-secondary text-white p-2"
-        >
-          <small>Condition</small>
-          <p class="mb-0">{{ this.getText() }} - {{ this.newIfValue }}</p>
-          <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-2">
-            <button @click="resetCondition" class="btn btn-outline-light btn-sm me-md-1" type="button"><i class="bi bi-trash"></i></button>
-            <button @click="toggleConditionShowForm" class="btn btn-outline-light btn-sm" type="button">
-              <i class="bi bi-pencil-fill"></i>
-            </button>
-          </div>
-        </div>
-
-        <small v-if="showForm && showConditionForm"
-          >Show this textblock only when the answer to the question
-        </small>
-        <div v-if="showForm && showConditionForm">
-          <div class="row g-3 mb-3">
-            <div class="col-sm-7">
-              <select
-                class="form-select"
-                v-model="newQuestion"
-                @change="options = getOptions()"
-              >
-                <option
-                  v-for="(question, index) in questions"
-                  :value="question.id"
-                  :key="`${index}`"
-                >
-                  {{ question.text }}
-                </option>
-              </select>
-            </div>
-            <div class="col-sm-3" v-if="newQuestion !== ''">
-              <select class="form-select" v-model="newIfValue">
-                <option
-                  v-for="(optionValue, optionKey, index) in options"
-                  :value="optionValue"
-                  :key="`${index}`"
-                >
-                  {{ optionValue }}
-                </option>
-              </select>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div class="card-body bg-light">
+      <div v-if="showForm">
         <editor
           v-model="renderedContentEdited"
           :init="{
@@ -120,15 +73,78 @@
            bullist numlist outdent indent | removeformat | help',
           }"
         />
-        <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3">
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3 mb-1">
           <button
-            class="btn btn-outline-danger btn-sm me-md-2"
+            class="btn btn-outline-light btn-sm me-md-2"
             @click="cancel"
             type="button"
           >
             cancel
           </button>
-          <button class="btn btn-primary btn-sm" @click="save" type="button">
+          <button class="btn btn-success btn-sm" @click="save" type="button">
+            save
+          </button>
+        </div>
+      </div>
+
+      <div v-if="showConditionForm">
+        <div class="bg-white" :style="getformStyles()">
+          <div
+            class="alert alert-primary d-flex align-items-center rounded-0"
+            role="alert"
+          >
+            <p v-if="!newIfValue">This Textblock is always displayed.</p>
+            <p v-if="newQuestion && newIfValue">This Textblock only displayed when the answer to "{{ this.getText() }}" is  "{{ this.newIfValue }}".</p>
+          </div>
+          <div>
+            <div class="row g-3 mb-3 mx-1">
+              <div class="col-sm-7">
+                <select
+                  class="form-select"
+                  v-model="newQuestion"
+                  @change="options = getOptions()"
+                >
+                  <option
+                    v-for="(question, index) in questions"
+                    :value="question.id"
+                    :key="`${index}`"
+                  >
+                    {{ question.text }}
+                  </option>
+                </select>
+              </div>
+              <div class="col-sm-3" v-if="newQuestion !== ''">
+                <select class="form-select" v-model="newIfValue">
+                  <option
+                    v-for="(optionValue, optionKey, index) in options"
+                    :value="optionValue"
+                    :key="`${index}`"
+                  >
+                    {{ optionValue }}
+                  </option>
+                </select>
+              </div>
+              <div class="col-sm-3" v-if="newQuestion !== ''">
+                <button
+                  @click="resetCondition"
+                  class="btn btn-outline-light btn-sm me-md-1"
+                  type="button"
+                >
+                  <i class="bi bi-trash"></i>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="d-grid gap-2 d-md-flex justify-content-md-end mt-3 mb-1">
+          <button
+            class="btn btn-outline-light btn-sm me-md-2"
+            @click="cancel"
+            type="button"
+          >
+            cancel
+          </button>
+          <button class="btn btn-success btn-sm" @click="save" type="button">
             save
           </button>
         </div>
@@ -229,7 +245,11 @@ export default {
       this.showConditionForm = !this.showConditionForm;
     },
     cancel: function () {
-      this.showForm = !this.showForm;
+      this.showForm = false;
+      this.showConditionForm = false;
+      this.hover = false;
+      this.newQuestion = this.question;
+      this.newIfValue = this.if_value;
       this.renderedContentEdited = this.renderedContent;
     },
     save: function () {
@@ -255,7 +275,9 @@ export default {
         .then((data) => {
           this.renderedContent = data.content;
           this.renderedContentEdited = data.content;
-          this.showForm = !this.showForm;
+          this.showForm = false;
+          this.showConditionForm = false;
+          this.hover = false;
           this.textblockid = data.id;
         });
     },
