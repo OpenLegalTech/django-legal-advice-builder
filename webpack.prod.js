37e2b6bef77e28c9d009f7221bb3c52f62d66f42
Magdalena Noffke <magdalena.noffke@googlemail.com>
2021-10-25 17:02:05 +0200

    rebuild production static fiels

---
+++ webpack.prod.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge')
+const common = require('./webpack.common.js')
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        terserOptions: {
+          ecma: 5
+        }
+      })
+    ]
+  }
+})
